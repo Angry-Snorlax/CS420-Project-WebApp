19e71d9550644a8a4949814870b5b1aa48d13479
angry_snorlax <lemaye@thomas.edu>
2019-04-29 15:58:33 -0400

    Testing

--- CS420-Project-WebApp/Program.cs
+++ CS420-Project-WebApp/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using Newtonsoft.Json;
+using CS420_Project_WebApp;
 
 namespace CS420_Project_WebApp
 {
@@ -15,10 +18,42 @@ namespace CS420_Project_WebApp
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            using (WebClient client = new WebClient())
+            {
+                string thina = client.DownloadString("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=EVUWUA3ID1W5BEQF");
+                JsonParse items = JsonConvert.DeserializeObject<JsonParse>(thina);
+                items.Display();
+            }
         }
+        /*public static string testing()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=EVUWUA3ID1W5BEQF");
+                return parseTest(json);
+            }
+        }
+        public static string parseTest(string testing)
+        {
+            string josn = JsonConvert.DeserializeObject<string>(testing);
+            List<string> jon;
+            foreach (var item in josn.)
+            {
+                jon.Add(item. )
+            }
+        }*/
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        public static string Parse()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string thina = client.DownloadString("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=EVUWUA3ID1W5BEQF");
+                JsonParse items = JsonConvert.DeserializeObject<JsonParse>(thina);
+                return items.Display();
+            }
+        }
     }
 }
---
+++ CS420-Project-WebApp/jsonParse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CS420_Project_WebApp
+{
+    public class JsonParse
+{
+        public static string title;
+        public static string fCurCodeTitle;
+        public static string fCurCode;
+        public static string fCurNameTitle;
+        public static string fCurName;
+        public static string toCurCodeTitle;
+        public static string toCurCode;
+        public static string toCurNameTitle;
+        public static string toCurName;
+        public static string excRateTitle;
+        public static string excRate;
+        public static string lastRefTitle;
+        public static string lastRef;
+        public static string timeTitle;
+        public static string time;
+
+        public JsonParse(string a, string b, string c, string d, string e, string f, string g, string h, string i, string j, string k, string l, string m, string n, string o)
+        {
+            title = a;
+            fCurCodeTitle = b;
+            fCurCode = c;
+            fCurNameTitle = d;
+            fCurName = e;
+            toCurCodeTitle = f;
+            toCurCode = g;
+            toCurNameTitle = h;
+            toCurName = i;
+            excRateTitle = j;
+            excRate = k;
+            lastRefTitle = l;
+            lastRef = m;
+            timeTitle = n;
+            time = o;
+        }
+        public string Display()
+        {
+            return excRate;
+        }
+}
+}
